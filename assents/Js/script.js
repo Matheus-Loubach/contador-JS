3e64bf96b85eb6ff255bf1bc9289b8f7de407352
loubach <34719454+Matheus-Loubach@users.noreply.github.com>
2022-05-23 20:19:36 -0300

    Add files via upload

--- assents/Js/script.js
+++ assents/Js/script.js
@@ -4,9 +4,32 @@ var currentNumber = 0;
 function increment(){
   currentNumber = currentNumber + 1;
   currentNumberWrapper.innerHTML = currentNumber;
+  color();
 }
 
 function decrement(){
   currentNumber = currentNumber - 1;
   currentNumberWrapper.innerHTML = currentNumber;
+  color();
+
+}
+
+function color(){
+
+
+
+  if(currentNumber <= -1)
+  {
+    document.getElementById('currentNumber').style.color = "red";
+  }
+
+  else if(currentNumber == 0){
+    document.getElementById('currentNumber').style.color = "green";
+  }
+
+  else{
+    document.getElementById('currentNumber').style.color = "black";
+  }
+
+  
 }
